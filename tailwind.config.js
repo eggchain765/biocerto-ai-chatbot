d15d0a7311e6f1cc04fae0b3977f3c02133ad9f2
eggchain765 <fabio.fabiani@eggchain.net>
2025-05-16 23:57:54 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,1 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        biocerto: "#1e446e",
-        "biocerto-light": "#48c4ca"
-      },
-      fontFamily: {
-        sans: ["Arial", "sans-serif"]
-      }
-    },
-  },
-  plugins: [],
-}
+
